c33207b852be87e2bd5372ed382df61f3ec18919
Asebodi <aditraharditya45@gmail.com>
2020-12-14 16:10:13 +0700

    Initialized Firestore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase";
+import { FirestoreProvider } from "@react-firebase/firestore";
+import config from "../firebase";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirestoreProvider {...config} firebase={firebase}>
+      <App />
+    </FirestoreProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
