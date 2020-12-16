958268ab4a12234e67a06be150af647dabd329fa
Asebodi <aditraharditya45@gmail.com>
2020-12-16 21:28:34 +0700

    Implemented react router and imported assets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ItemReg from "./components/ItemReg";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Cart from "./components/pages/Cart";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App p-6">
-      <ItemReg />
-    </div>
+    <Router>
+      <div className="App p-6">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
