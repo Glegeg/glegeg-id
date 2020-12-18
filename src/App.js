d0680255663a69596af873d2f7b5dcb46afed6ea
Asebodi <aditraharditya45@gmail.com>
2020-12-18 23:27:42 +0700

    Added Nav component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
 import Cart from "./components/pages/Cart";
 import Home from "./components/pages/Home";
 import Faq from "./components/pages/Faq";
 import Order from "./components/pages/Order";
+import Nav from "./components/Nav";
+import Product from "./components/pages/Product";
+
+// function usePage() {
+//   let loc = useLocation();
+
+//   useEffect(() => {
+//     console.log(loc.pathname);
+//   }, [loc]);
+// }
 
 function App() {
+  // usePage();
+
   return (
     <Router>
-      <div className="App p-6">
-        <Route path="/" exact>
-          <Home />
-        </Route>
-
-        <Route path="/cart" exact>
-          <Cart />
-        </Route>
-
-        <Route path="/faq" exact>
-          <Faq />
-        </Route>
-
-        <Route path="/order" exact>
-          <Order />
-        </Route>
+      <div className="App relative">
+        <Route component={WithNav} />
+        <Route component={WithoutNav} />
       </div>
     </Router>
+  );
+}
+
+function WithoutNav() {
+  return (
+    <div className="container">
+      <Route path="/order" exact>
+        <Order />
+      </Route>
+
+      <Route path="/product" exact>
+        <Product />
+      </Route>
+    </div>
+  );
+}
+
+function WithNav() {
+  return (
+    <div className="home">
+      <Nav />
+      <Route path="/" exact>
+        <Home />
+      </Route>
+
+      <Route path="/cart" exact>
+        <Cart />
+      </Route>
+
+      <Route path="/faq" exact>
+        <Faq />
+      </Route>
+    </div>
   );
 }
 
