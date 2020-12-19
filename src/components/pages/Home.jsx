ab83b956ad0495d415bef306675fe80605ace510
Asebodi <aditraharditya45@gmail.com>
2020-12-19 14:27:48 +0700

    Time-based greeting

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,12 +7,40 @@ import HomeHighlight from "../HomeHighlight";
 export default function Home(props) {
   useEffect(() => {
     props.setNav(true);
-  }, []);
+  }, [props]);
+
+  const date = new Date();
+  const currentHour = date.getHours();
+  console.log(currentHour);
+
+  function outputHour(hour) {
+    switch (hour) {
+      case hour >= 0 && hour < 4:
+        return "Pagi kak!";
+
+      case hour >= 4 && hour < 10:
+        return "Pagi kak!";
+
+      case hour >= 10 && hour < 14:
+        return "Siang kak!";
+
+      case hour >= 14 && hour < 18:
+        return "Sore kak!";
+
+      case hour >= 18 && hour < 24:
+        return "Malam kak!";
+
+      default:
+        return "Pagi kak!";
+    }
+  }
 
   return (
     <div className="p-6">
       <img src={logo} alt="" className="w-24 text-center block m-auto" />
-      <h4 className="font-bold mt-4 text-lg text-heading">Pagi kak!</h4>
+      <h4 className="font-bold mt-4 text-lg text-heading">
+        {outputHour(currentHour)}
+      </h4>
       <p className="text-defocus">Mari dibeli gleGeg nya 😁</p>
 
       <HomeHighlight />
