a471bd70d29611cf66a06958849748eab256c884
Asebodi <aditraharditya45@gmail.com>
2020-12-22 20:55:46 +0700

    Added individual product page

---
+++ src/components/products/ChocoDelfi.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function ChocoDelfi(props) {
+  useEffect(() => {
+    setNav(false);
+  }, [setNav]);
+
+  return (
+    <div>
+      <ProductGallery />
+
+      <div className="p-6">
+        <h2 className="text-heading text-2xl font-bold">{product.name}</h2>
+        {product.desc.map((item, idx) => (
+          <p key={idx} className="text-gray-600 text-sm mt-4">
+            {item}
+          </p>
+        ))}
+
+        <Info preorder="15 - 32 Desember 2020" />
+
+        <ProductCheckout price={product.price} />
+      </div>
+    </div>
+  );
+}
+
+export default ChocoDelfi;
