1dfc23dbe1d44dc8230554c79b456d78c02adfa6
Asebodi <aditraharditya45@gmail.com>
2021-04-04 15:12:46 +0700

    Fixed time based greeting in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -14,33 +14,28 @@ export default function Home(props) {
   // console.log(currentHour);
 
   function outputHour(hour) {
-    switch (hour) {
-      case hour >= 0 && hour < 4:
-        return "Pagi kak!";
-
-      case hour >= 4 && hour < 10:
-        return "Pagi kak!";
-
-      case hour >= 10 && hour < 14:
-        return "Siang kak!";
-
-      case hour >= 14 && hour < 18:
-        return "Sore kak!";
-
-      case hour >= 18 && hour < 24:
-        return "Malam kak!";
-
-      default:
-        return "Pagi kak!";
+    console.log(hour);
+    if (hour >= 0 && hour < 4) {
+      return "Pagi kak!";
+    }
+    if (hour >= 4 && hour < 10) {
+      return "Pagi kak!";
+    }
+    if (hour >= 10 && hour < 14) {
+      return "Siang kak!";
+    }
+    if (hour >= 14 && hour < 18) {
+      return "Sore kak!";
+    }
+    if (hour >= 18 && hour < 24) {
+      return "Malam kak!";
     }
   }
 
   return (
     <div className="p-6">
       <img src={logo} alt="" className="w-24 text-center block m-auto" />
-      <h4 className="font-bold mt-4 text-lg text-heading">
-        {outputHour(currentHour)}
-      </h4>
+      <h4 className="font-bold mt-4 text-lg text-heading">{outputHour(10)}</h4>
       <p className="text-defocus">Mari dibeli gleGeg nya 😁</p>
 
       <HomeHighlight />
