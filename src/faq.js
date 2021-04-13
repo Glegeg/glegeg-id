649e84bf5010fe23aaa6b13896020fc401b30843
Asebodi <aditraharditya45@gmail.com>
2021-04-13 08:59:42 +0700

    Fetch faq items from local file

---
+++ src/faq.js
@@ -0,0 +1,19 @@
+const faq = [
+  {
+    judul: "Bagaimana cara membeli gleGleg?",
+    isi: [
+      "1. Buka katalog produk kami di halaman beranda.",
+      "2. Klik pesan dalam halaman produk.",
+      "3. Isi data diri dan klik tombol pesan.",
+      "4. Detail pesanan akan dikirimkan melalui Whatsapp.",
+    ],
+  },
+  {
+    judul: "Kapan periode pre-order gleGleg?",
+    isi: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in suscipit nisl, nec vulputate velit. Praesent ut libero nec lorem facilisis pellentesque sagittis at risus.",
+    ],
+  },
+];
+
+export default faq;
